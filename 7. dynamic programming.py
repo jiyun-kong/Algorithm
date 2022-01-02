dd64a9fc61f4acd9eff06faa09383382647236e3
공지윤 <62550230+jiyun-kong@users.noreply.github.com>
2022-01-02 23:18:17 +0900

    동적 프로그래밍

--- 7. dynamic programming.py
+++ 7. dynamic programming.py
@@ -264,3 +264,23 @@ for tc in range(int(input())):
         result = max (result, dp[i][m-1])
     
     print(result)
+
+   
+# <문제> 병사 배치하기
+# N명의 병사가 무작위로 나열되어 있습니다. 각 병사는 특정한 값의 전투력을 보유하고 있습니다. 병사를 배치할 때에는 전투력이 높은 병사가 앞쪽에 오도록 내림차순으로 배치를 하고자 합니다. 
+# 다시 말해 앞쪽에 있는 병사의 전투력이 항상 뒤쪽에 있는 병사보다 높아야 합니다.
+# 또한 배치 과정에서는 특정한 위치에 있는 병사를 열외시키는 방법을 이용합니다. 그러면서도 남아 있는 병사의 수가 최대가 되도록 하고 싶습니다.
+# 병사에 대한 정보가 주어졌을 때, 남아 있는 병사의 수가 최대가 되도록 하기 위해서 열외시켜야 하는 병사의 수를 출력하는 프로그램을 작성하세요.
+
+n = int(input())
+array = list(map(int, input().split()))
+array.reverse()
+
+dp = [1] * n
+
+for i in range(1, n):
+    for j in range(0,i):
+        if array[j] < array[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+            
+print(n - max(dp))
