48172902dfb7bb170cec600eff6d2338070ff44d
공지윤 <62550230+jiyun-kong@users.noreply.github.com>
2022-02-22 11:49:37 +0900

    Delete 11650.py

--- 11650.py
+++
@@ -1,9 +0,0 @@
-coordinate = []
-
-for i in range(int(input())):
-    coordinate.append(list(map(int, input().split(' '))))
-
-coordinate.sort(key=lambda x: (x[0], x[1]))
-
-for co in coordinate:
-    print(co[0], co[1])
